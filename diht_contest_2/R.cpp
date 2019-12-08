dc157ff107b45b6b747b223f12dd4fbfa8dc2be6
brainabuser <ghost23rus@gmail.com>
2019-12-08 16:07:29 +0300

    Several simple problems solved

--- diht_contest_2/R.cpp
+++ diht_contest_2/R.cpp
@@ -1,4 +1,30 @@
-//
-// Created by Vladimir on 08.12.2019.
-//
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int *array = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> array[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            if (array[i] > array[j]) {
+                swap(array[i], array[j]);
+            }
+        }
+    }
+    int *answer = new int[n];
+    answer[0] = 0;
+    answer[1] = array[1] - array[0];
+    for (int i = 2; i < n; ++i) {
+        answer[i] = array[i] - array[i - 1];
+        answer[i] += (i == 2) ? answer[i - 1] : min(answer[i - 2], answer[i - 1]);
+    }
+    cout << answer[n - 1] << endl;
+    delete[] array;
+    delete[] answer;
+    return 0;
+}
