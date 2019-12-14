2f973886b33d7009fa44ba36d52381aa4a464326
brainabuser <ghost23rus@gmail.com>
2019-12-14 03:13:02 +0300

    Problem S solved

--- diht_contest_2/S.cpp
+++ diht_contest_2/S.cpp
@@ -2,21 +2,55 @@
 
 using namespace std;
 
+void reverse(int *array, const int &n);
+
 int main() {
     int n;
     cin >> n;
-    int *max = new int[n];
     int *array = new int[n];
-    cin >> array[0];
-    int max_size = 0;
-    int temp_size = 1;
-    int *temp = new int[n];
-    temp[0] = array[0];
-    for (int i = 1; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cin >> array[i];
     }
+    int *length = new int[n];
+    int *prev = new int[n];
+    int max_length = 1;
+    int max_index = 0;
+    for (int i = 0; i < n; ++i) {
+        length[i] = 1;
+        prev[i] = -1;
+        for (int j = 0; j < i; ++j) {
+            if (array[j] < array[i] && length[j] + 1 > length[i]) {
+                length[i] = length[j] + 1;
+                prev[i] = j;
+            }
+            if (length[i] > max_length) {
+                max_length = length[i];
+                max_index = i;
+            }
+        }
+    }
+    int pos = max_index;
+    int *answer = new int[max_length];
+    int cnt = 0;
+    while(pos != -1){
+        answer[cnt++] = array[pos];
+        pos = prev[pos];
+    }
     delete[] array;
-    delete[] temp;
-    delete[] max;
+    reverse(answer, max_length);
+    for(int i = 0; i < max_length; ++i){
+        cout << answer[i] << ' ';
+    }
+    delete[] answer;
+    delete[] length;
+    delete[] prev;
     return 0;
+}
+
+void reverse(int *array, const int &n){
+    for(int i = 0; i < n / 2; ++i){
+        int temp = array[i];
+        array[i] = array[n - i - 1];
+        array[n - i - 1] = temp;
+    }
 }
--- diht_contest_2/main.cpp
+++ diht_contest_2/main.cpp
@@ -1,72 +1,27 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
-
-struct Contestant {
-    int grades = -1;
-    int id = -1;
-};
-
-void readArray(Contestant *array, const int &size);
-
-void printArray(Contestant *array, const int &size);
-
-void quickSort(Contestant *array, const int &left, const int &right);
 
 int main() {
     int n;
     cin >> n;
-    auto *competitors = new Contestant[n];
-    readArray(competitors, n);
-    quickSort(competitors, 0, n - 1);
-    printArray(competitors, n);
-    delete[] competitors;
-    return 0;
-}
-
-void readArray(Contestant *array, const int &size) {
-    for (int i = 0; i < size; ++i) {
-        cin >> array[i].id >> array[i].grades;
+    int *array = new int[n];
+    int *answer = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> array[i];
+        answer[i] = 1;
     }
-}
-
-void printArray(Contestant *array, const int &size) {
-    for (int i = 0; i < size; ++i) {
-        cout << array[i].id << ' ' << array[i].grades << endl;
-    }
-}
-
-int partition(Contestant *array, const int &left, const int &right) {
-    int pivot = array[(left + right) / 2].grades;
-    int i = left - 1;
-    int j = right + 1;
-    while (true) {
-        do {
-            ++i;
-        } while (array[i].grades > pivot);
-        do {
-            --j;
-        } while (array[j].grades < pivot);
-        if (i >= j) {
-            return j;
-        }
-        if (array[i].grades == array[j].grades) {
-            if(array[i].id > array[j].id){
-                swap(array[i].id, array[j].id);
-            }else{
-                swap(array[i].grades, array[j].grades);
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (array[j] < array[i]) {
+                answer[i] += answer[j];
             }
-        } else {
-            swap(array[i], array[j]);
         }
+        sum += answer[i] % 1000000;
     }
-}
-
-void quickSort(Contestant *array, const int &left, const int &right) {
-    if (left < right) {
-        int pivot = partition(array, left, right);
-        quickSort(array, left, pivot);
-        quickSort(array, pivot + 1, right);
-    }
+    cout << sum << endl;
+    delete[] array;
+    delete[] answer;
+    return 0;
 }
