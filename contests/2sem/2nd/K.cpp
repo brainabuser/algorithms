5b40e19b2af585db313aa7cdb428a79eadcf15db
brainabuser <ghost23rus@gmail.com>
2020-04-27 11:36:38 +0300

    K problem solved

--- contests/2sem/2nd/K.cpp
+++ contests/2sem/2nd/K.cpp
@@ -11,14 +11,12 @@ using namespace std;
 int main() {
     long double p, h_p, s, h_s;
     cin >> p >> h_p >> s >> h_s;
-    long double x;
-    long double tg_alpha;
     cout << fixed << setprecision(7);
     if (s == p) {
         cout << s << endl;
     } else {
-        x = ((s > p) ? (h_s - h_p) / (1. - p / s) : (h_s - h_p) / (p / s - 1.));
-        tg_alpha = 1. / sqrt((pow(x / s, 2) - 1.));
+        long double x = ((s > p) ? (h_s - h_p) / (1. - p / s) : (h_s - h_p) / (p / s - 1.));
+        long double tg_alpha = 1. / sqrt((pow(x / s, 2) - 1.));
         long double result = ((s > p) ? (x - h_s) * tg_alpha : (x + h_s) * tg_alpha);
         cout << ((result < 0.0) ? 0.0 : result) << endl;
     }
