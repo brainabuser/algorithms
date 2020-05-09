7a0b489318dcf21e4b1b2fc37ceeb7d243004f20
brainabuser <ghost23rus@gmail.com>
2020-05-10 02:24:41 +0300

    B task solved

---
+++ contests/2sem/3rd/B.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Vladimir on 10.05.2020.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> beasts(n);
+    for (auto &t : beasts) {
+        cin >> t;
+    }
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        auto lower = lower_bound(beasts.begin(), beasts.end(), temp);
+        if (lower == beasts.end()) {
+            cout << 0 << endl;
+        } else {
+            cout << upper_bound(lower, beasts.end(), temp) - lower << endl;
+        }
+    }
+    return 0;
+}
