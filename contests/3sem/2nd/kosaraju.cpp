51f131402e3d056635e84a675d88e9cb5900600e
v0vch4nsky <ghost23rus@gmail.com>
2020-11-01 14:02:24 +0300

    2nd contest done

--- contests/3sem/2nd/kosaraju.cpp
+++ contests/3sem/2nd/kosaraju.cpp
@@ -3,17 +3,36 @@
 //
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <deque>
 
 using namespace std;
 
+enum Color {
+    WHITE, GRAY, BLACK
+};
+
 struct Graph {
     explicit Graph(int size) {
-        vertices.resize(size);
+        adjacency_list.resize(size);
     }
-    vector<vector<int>> vertices;
+    void read(const int &edge_num) {
+        for (int j = 0; j < edge_num; ++j) {
+            int from, to;
+            cin >> from >> to;
+            adjacency_list[from - 1].push_back(to);
+        }
+    }
+    Graph getTransposed() const {
+        Graph transposed(adjacency_list.size());
+        for (int i = 0; i < transposed.adjacency_list.size(); ++i) {
+            for (int j = 0; j < adjacency_list[i].size(); ++j) {
+                transposed.adjacency_list[adjacency_list[i][j] - 1].push_back(i + 1);
+            }
+        }
+        return transposed;
+    }
+    vector<vector<int>> adjacency_list;
 };
 
 class AbstractVisitor {
@@ -27,86 +46,86 @@ class TopSortVisitor : public AbstractVisitor {
   public:
     explicit TopSortVisitor(int size) {
         order.resize(size);
+        for (int i = 0; i < size; ++i) {
+            order[i] = i;
+        }
     }
-    void start() {}
+    void start() override {}
     void process_vertex(const int &vertex) override {
         result.push_front(vertex);
     }
+  private:
     deque<int> result;
+
+    friend class CondensationVisitor;
 };
 
 class CondensationVisitor : public AbstractVisitor {
   public:
-    explicit CondensationVisitor(int size) : component_id(0) {
+    explicit CondensationVisitor(int size, const TopSortVisitor &visitor) : component_id(0) {
         result.resize(size);
         order.resize(size);
+        for (int i = 0; i < size; ++i) {
+            order[i] = visitor.result[i] - 1;
+        }
     }
-    void start() {
+    void start() override {
         ++component_id;
     }
     void process_vertex(const int &vertex) override {
         result[vertex - 1] = component_id;
     }
+    int getComponentId() const {
+        return component_id;
+    }
+    void print_vertices() const {
+        for (const int &i : result) {
+            cout << i << ' ';
+        }
+        cout << endl;
+    }
+  private:
     int component_id;
     vector<int> result;
 };
 
-void DFS(const Graph &graph, AbstractVisitor &vis);
-
-void read_graphs(Graph &graph, Graph &graph_t, const int &edge_num) {
-    for (int j = 0; j < edge_num; ++j) {
-        int from, to;
-        cin >> from >> to;
-        graph.vertices[from - 1].push_back(to);
-        graph_t.vertices[to - 1].push_back(from);
-    }
-}
-
-void print_answer(const CondensationVisitor &strong_components, const int &vertex_num) {
-    cout << strong_components.component_id << endl;
-    for (int i = 0; i < vertex_num; ++i) {
-        cout << strong_components.result[i] << ' ';
-    }
-    cout << endl;
-}
+void getStrongComponents(const Graph &graph);
 
 int main() {
     int n, m;
     cin >> n >> m;
     Graph graph(n);
-    Graph graph_t(n);
-    read_graphs(graph, graph_t, m);
-    TopSortVisitor sorted_graph(n);
-    for (int i = 0; i < n; ++i) {
-        sorted_graph.order[i] = i + 1;
-    }
-    DFS(graph, sorted_graph);
-    CondensationVisitor strong_components(n);
-    for (int i = 0; i < n; ++i) {
-        strong_components.order[i] = sorted_graph.result[i];
-    }
-    DFS(graph_t, strong_components);
-    print_answer(strong_components, n);
+    graph.read(m);
+    getStrongComponents(graph);
     return 0;
 }
 
-void DFS_Visit(const Graph &graph, AbstractVisitor &vis, vector<string> &color, const int &root) {
-    color[root - 1] = "gray";
-    for (const auto &u : graph.vertices[root - 1]) {
-        if (color[u - 1] == "white") {
-            DFS_Visit(graph, vis, color, u);
+void DFS_Visit(const Graph &graph, AbstractVisitor &visitor, vector<Color> &color, const int &vertex_id) {
+    color[vertex_id] = GRAY;
+    for (const auto &u : graph.adjacency_list[vertex_id]) {
+        if (color[u - 1] == WHITE) {
+            DFS_Visit(graph, visitor, color, u - 1);
         }
     }
-    color[root - 1] = "black";
-    vis.process_vertex(root);
+    color[vertex_id] = BLACK;
+    visitor.process_vertex(vertex_id + 1);
 }
 
-void DFS(const Graph &graph, AbstractVisitor &vis) {
-    vector<string> color(graph.vertices.size(), "white");
-    for (const auto &i : vis.order) {
-        if (color[i - 1] == "white") {
-            vis.start();
-            DFS_Visit(graph, vis, color, i);
+void DFS(const Graph &graph, AbstractVisitor &visitor) {
+    vector<Color> color(graph.adjacency_list.size(), WHITE);
+    for (const auto &i : visitor.order) {
+        if (color[i] == WHITE) {
+            visitor.start();
+            DFS_Visit(graph, visitor, color, i);
         }
     }
+}
+
+void getStrongComponents(const Graph &graph) {
+    TopSortVisitor sorted_graph(graph.adjacency_list.size());
+    DFS(graph, sorted_graph);
+    CondensationVisitor strong_components(graph.adjacency_list.size(), sorted_graph);
+    DFS(graph.getTransposed(), strong_components);
+    cout << strong_components.getComponentId() << endl;
+    strong_components.print_vertices();
 }
